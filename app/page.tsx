8ad85f4c661a01bbb3b5490d8952a060cc087f9c
localscripts <bronysmith9@gmail.com>
2025-08-05 08:13:13 -0700

    Clear all files from repository

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import JsonBuilder from "@/components/json-builder"
-
-export default function HomePage() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-4 md:p-8">
-      <JsonBuilder />
-    </main>
-  )
-}
